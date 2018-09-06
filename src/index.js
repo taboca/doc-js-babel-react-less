2b707148f8038a7c27de31bf96e2b368732b9740
Marcio Galli <taboca@gmail.com>
2018-09-06 18:26:40 -0300

    initial files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+window.mainAppComponent = ReactDOM.render((
+    <App/>
+), document.getElementById('root'));
